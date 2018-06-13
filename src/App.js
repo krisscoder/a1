32ddba0969182707c4eea2d84d39a46b414f0f59
kriss <cristiancarrera@gmail.com>
2018-06-13 17:37:54 -0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Navigation from  './components/Navigation.js';
+import { todos } from './todos.json'
 
 class App extends Component {
+  //construye estado de la app y guarda datos de json
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+  }
+
   render() {
+    //procesa datos de json en un loop
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+         <div className="card mt-4">
+            <div className="card-header">
+              <h3>{ todo.title }</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                { todo.priority }
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{ todo.description }</p>
+              <p><mark>{ todo.responsible }</mark></p>
+            </div>
+           </div>
+        </div>
+      )
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navigation title="Task" len={ this.state.todos.length }/>
+        <div className="container">
+          <div className="row">{ todos }</div>
+        </div>
       </div>
     );
   }
